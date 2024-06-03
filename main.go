66a9626d4c4e1e6d90a79fc2d232a75497e698b2
Ronikwear <ijalilov92@gmail.com>
2024-06-03 16:04:20 +0500

    2nd commit

--- main.go
+++ main.go
@@ -1,28 +1,24 @@
 package main
 
 import (
-	"log"
+	"bytes"
 	"net/http"
 )
 
-// Создается функция-обработчик "home", которая записывает байтовый слайс, содержащий
-// текст "Привет из Snippetbox" как тело ответа.
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Привет из Snippetbox"))
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+
+
+	if r.Method != http.MethodPost {
+
+
+
+
+		w.WriteHeader(405)
+		w.Write([]byte("GET-Метод запрещен!"))
+		return
 }
 
-func main() {
-	// Используется функция http.NewServeMux() для инициализации нового рутера, затем
-	// функцию "home" регистрируется как обработчик для URL-шаблона "/".
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+w.Write([]byte("Создание новой заметки..."))
 
-	// Используется функция http.ListenAndServe() для запуска нового веб-сервера.
-	// Мы передаем два параметра: TCP-адрес сети для прослушивания (в данном случае это "localhost:4000")
-	// и созданный рутер. Если вызов http.ListenAndServe() возвращает ошибку
-	// мы используем функцию log.Fatal() для логирования ошибок. Обратите внимание
-	// что любая ошибка, возвращаемая от http.ListenAndServe(), всегда non-nil.
-	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
 }
